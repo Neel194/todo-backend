a4af33c3a4ba98820f07fa2917ec2ccab1d5e508
Neel1992004 <pneel1924@gmail.com>
2025-07-17 11:58:20 +0530

    Implement createUser controller and route with Zod input validation

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from 'express';
+import User from '../models/user.model';
+import { createUserScehma } from '../validations/user.validation';
+
+// create user controller
+export const createUser = async (
+    req: Request,
+    res: Response
+): Promise<void> => {
+    // validate input using zod
+    const parsed = createUserScehma.safeParse(req.body);
+
+    if (!parsed.success) {
+        // validation fails, send error with details
+        res.status(400).json({ error: parsed.error.issues });
+        return;
+    }
+    try {
+        const { name, email, password, role } = req.body;
+
+        // check if email already exist
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            res.status(409).json({
+                error: 'User already exists with this email',
+            });
+            return;
+        }
+
+        // create user
+        const user = await User.create({
+            name,
+            email,
+            password,
+            role: role || 'user',
+        });
+
+        // send success response
+        res.status(201).json({ message: 'User created successfully', user });
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/db';
+import userRoutes from '../src/routes/user.route';
+dotenv.config();
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/users', userRoutes);
+
+// MongoDB database connect and server start
+connectDB().then(() => {
+    app.listen(process.env.PORT, () =>
+        console.log(`🚀 Server running at http://localhost:${process.env.PORT}`)
+    );
+});
